749270431a09614cf88754d446f874fc1f26bb44
Jasper Precht <72135430+jasperp92@users.noreply.github.com>
2025-05-26 17:56:43 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 phSensor.setSamples(10)
 basic.forever(function () {
     basic.showNumber(phSensor.readPh(AnalogPin.C16))
-    serial.writeLine("" + (Math.round(phSensor.readPh(AnalogPin.C16))))
-    basic.pause(1000)
+    serial.writeLine("" + (phSensor.readPh(AnalogPin.C16)))
+    basic.pause(100)
 })
